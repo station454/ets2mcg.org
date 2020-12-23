0c458c8887a6a9921c747f488cf1bb6edf3e56bb
User <10652364+dhkim0800@users.noreply.github.com>
2020-12-23 12:54:42 +0900

    A whole new website

--- common/js/basehelper.js
+++ common/js/basehelper.js
@@ -1,16 +1,15 @@
 function loadSlideshowSlides() {
-    var slideshowHome = "../common/img/slides/";
-    var slideshowList = "show.json";
-    var slideshowElem = "#slideshow";
+    return new Promise((resolve) => {
+        var slideshowHome = "https://common.ets2mcg.org/img/slides/";
+        var slideshowList = "show.json";
+        var slideshowElem = "#slideshow";
 
-    var element = $(slideshowElem);
-    $.ajax(slideshowHome + slideshowList, {
-        method: "GET",
-        async: false,
-        success: (data) => {
+        var element = $(slideshowElem);
+        $.getJSON(slideshowHome + slideshowList, (data) => {
             data.forEach(o => {
                 $("<img />").addClass("imgSlide").css("display", "none").attr("src", slideshowHome + o).appendTo(element);
             });
-        }
+            resolve();
+        });
     })
 }
